e10654fa8fe2781ef458c656f0b6b0889dd95a27
KO-YAMAKO <ko.foot1103@gmail.com>
2018-07-05 15:55:12 +0900

    add alamofire, success to get data in JSON

--- githubAPISample/ListViewController.swift
+++ githubAPISample/ListViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        WebAPI.get()
     }
 
     override func didReceiveMemoryWarning() {
--- githubAPISample/WebAPI.swift
+++ githubAPISample/WebAPI.swift
@@ -7,29 +7,49 @@
 //
 
 import Foundation
+import Alamofire
 
-typealias Input = Request
 
-typealias Request = (
-    url: URL,
-    queries: [URLQueryItem],
-    headers: [String: String],
-    methodAndPayload: HTTPMethodAndPayload
-)
-
-enum HTTPMethodAndPayload {
-    case get
-    var method: String {
-        switch self {
-        case .get:
-            return "GET"
-        }
-    }
+class WebAPI {
     
-    var body: Data? {
-        switch self {
-        case .get:
-            return nil
+    static func get() {
+        Alamofire.request("https://api.github.com/events").responseJSON { (response) in
+            print("Request" + String(describing: response.result))
+            print("Request" + String(describing: response.response))
+            if let json = response.result.value {
+                print("JSON: \(json)")
+            }
         }
     }
 }
+
+
+//
+//typealias Input = Request
+//
+//typealias Request = (
+//    url: URL,
+//    queries: [URLQueryItem],
+//    headers: [String: String],
+//    methodAndPayload: HTTPMethodAndPayload
+//)
+//
+//enum HTTPMethodAndPayload {
+//    case get
+//    var method: String {
+//        switch self {
+//        case .get:
+//            return "GET"
+//        }
+//    }
+//
+//    var body: Data? {
+//        switch self {
+//        case .get:
+//            return nil
+//        }
+//    }
+//}
+//
+//
+//
